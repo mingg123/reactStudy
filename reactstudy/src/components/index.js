b12e52a3f08bfe47cd1f6dcf980b3fa23c8d1235
minji_kim2 <minji_kim2@tmax.co.kr>
2021-07-23 16:34:06 +0900

    add : 성능 최적화 예시 1. React.memo사용 2. useReducer 사용

--- reactstudy/src/components/index.js
+++ reactstudy/src/components/index.js
@@ -1,57 +1,55 @@
-import React, { useRef, useState, useCallback } from "react";
+import React, { useRef, useState, useCallback, useReducer } from "react";
 import TodoInsert from "./TodoInsert";
 import TodoList from "./TodoList";
 import TodoTemplate from "./TodoTemplate";
 
 const TodoTempContainer = () => {
-  const [todos, setTodos] = useState([
-    {
-      id: 1,
-      text: "리액트의 기초 알아보기",
-      checked: true,
-    },
-    {
-      id: 2,
-      text: "컴포넌트 스타일링 해보기",
-      checked: false,
-    },
-    { id: 3, text: "일정 관리 앱 만들어보기", checked: false },
-  ]);
+  const [todos, dispatch] = useReducer(todoReducer, undefined, createBulkTodos);
+  //   const [todos, setTodos] = useState(createBulkTodos);
+  //   const [todos, setTodos] = useState([
+  //     {
+  //       id: 1,
+  //       text: "리액트의 기초 알아보기",
+  //       checked: true,
+  //     },
+  //     {
+  //       id: 2,
+  //       text: "컴포넌트 스타일링 해보기",
+  //       checked: false,
+  //     },
+  //     { id: 3, text: "일정 관리 앱 만들어보기", checked: false },
+  //   ]);
   const nextId = useRef(4);
-  const onInsert = useCallback(
-    (text) => {
-      const todo = {
-        id: nextId.current,
-        text,
-        checked: false,
-      };
-      setTodos(todos.concat(todo));
-      nextId.current += 1;
-    },
-    [todos]
-  );
-  const onRemove = useCallback(
-    (id) => {
-      setTodos(todos.filter((todo) => todo.id !== id));
-    },
-    [todos]
-  );
+  const onInsert = useCallback((text) => {
+    const todo = {
+      id: nextId.current,
+      text,
+      checked: false,
+    };
+    //   setTodos(todos.concat(todo));
+    //   setTodos((todos) => todos.concat(todo));
+    dispatch({ type: "INSERT", todo });
+    nextId.current += 1;
+  }, []);
+  const onRemove = useCallback((id) => {
+    dispatch({ type: "REMOVE", id });
+    // setTodos((todos) => todos.filter((todo) => todo.id !== id));
+  }, []);
 
-  const onToggle = useCallback(
-    (id) => {
-      setTodos(
-        todos.map((todo) =>
-          todo.id === id
-            ? {
-                ...todo,
-                checked: !todo.checked,
-              }
-            : todo
-        )
-      );
-    },
-    [todos]
-  );
+  const onToggle = useCallback((id) => {
+    dispatch({ type: "TOGGLE", id });
+    //   setTodos((todos) =>
+    //     todos.map((todo) =>
+    //       todo.id === id
+    //         ? {
+    //             ...todo,
+    //             checked: !todo.checked,
+    //           }
+    //         : todo
+    //     )
+    //   );
+  }, []);
+
   return (
     <TodoTemplate>
       <TodoInsert onInsert={onInsert} />
@@ -60,4 +58,30 @@ const TodoTempContainer = () => {
   );
 };
 
+function createBulkTodos() {
+  const array = [];
+  for (let i = 1; i <= 2500; i++) {
+    array.push({
+      id: i,
+      text: `할 일${i}`,
+      checked: false,
+    });
+  }
+  return array;
+}
+
+function todoReducer(todos, action) {
+  switch (action.type) {
+    case "INSERT":
+      return todos.concat(action.todo);
+    case "REMOVE":
+      return todos.filter((todo) => todo.id !== action.id);
+    case "TOGGLE":
+      return todos.map((todo) =>
+        todo.id === action.id ? { ...todo, checked: !todo.checked } : todo
+      );
+    default:
+      return todos;
+  }
+}
 export default TodoTempContainer;
