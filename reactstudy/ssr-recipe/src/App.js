9041e7efcd6c4d23e21ec97d7ea510ddcc282a58
mingg123 <spgb123spgb@gmail.com>
2022-02-04 20:52:45 +0900

    init Commit SSR example before eject

---
+++ reactstudy/ssr-recipe/src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+import Menu from './components/Menu';
+import RedPage from './pages/RedPage';
+import { Route, Routes } from 'react-router-dom';
+import BluePage from './pages/BluePage';
+
+function App() {
+  return (
+    <div>
+      <Menu />
+      <hr />
+      <Routes>
+        <Route path="/red" element={<RedPage />} />
+        <Route path="/blue" element={<BluePage />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
