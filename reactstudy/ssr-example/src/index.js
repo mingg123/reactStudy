8ae5b208ef6c34302e9c33cb0abb172580e41a9b
mingg123 <spgb123spgb@gmail.com>
2022-02-05 19:59:58 +0900

    feat : add mongoDB example

--- reactstudy/ssr-example/src/index.js
+++ reactstudy/ssr-example/src/index.js
@@ -1,5 +1,20 @@
+require('dotenv').config();
 const Koa = require('koa');
 const Router = require('koa-router');
+const bodyParser = require('koa-bodyparser');
+const mongoose = require('mongoose');
+
+const { PORT, MONGO_URI } = process.env;
+
+mongoose
+  .connect(MONGO_URI, { useNewUrlParser: true })
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch(e => {
+    console.error(e);
+  });
+
 const app = new Koa();
 const router = new Router();
 
@@ -8,11 +23,16 @@ const api = require('./api');
 //라우터 설정
 router.use('/api', api.routes());
 
+app.use(bodyParser());
+
 app.use(router.routes()).use(router.allowedMethods());
 
-app.listen(4000, () => {
-  console.log('Listening to port 4000');
+const port = PORT || 4000;
+
+app.listen(port, () => {
+  console.log('Listening to port %d', port);
 });
+
 // router.get('/', ctx => {
 //   ctx.body = '홈';
 // });
